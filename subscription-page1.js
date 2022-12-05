1c1413c1c3b33ff7d746fe74ac0bdc8c53b3b540
Cecilie Norrbohm <ceci332s@stud.kea.dk>
2022-12-05 16:58:04 +0100

    subscription page 1 - almost done

---
+++ subscription-page1.js
@@ -0,0 +1,9 @@
+// const button = document.getElementsByClass("dd-button-2");
+
+function buttonColor() {
+  document.getElementsByClass("dd-button-2").style.backgroundColor = "#92b9ab";
+}
+
+// button.addEventListener("click", function onClick() {
+//   button.style.backgroundColor = "#92b9ab";
+// });
