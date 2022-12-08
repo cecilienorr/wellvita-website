f79db52253e00cfff274925571ad7e238edf17f3
julieringaas <89411616+julieringaas@users.noreply.github.com>
2022-12-08 19:09:37 +0100

    mobile view form works

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+function myFunction() {
+  var checkBox = document.getElementById("myCheck");
+  var text = document.getElementById("text");
+  if (checkBox.checked == true) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+// CHANGING COLOR form
+
+// $("input[type=text]").change(function () {
+//   var filled = true;
+//   $("input[type=text]").each(function (index) {
+//     if ($(this).val() == "") {
+//       filled = false;
+//     }
+//     if (filled === false) {
+//       //change color
+//     }
+//   });
+// });
