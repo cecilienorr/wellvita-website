45f237dcc3df13d31e41c702fb104904550692ca
Annika Flyxe <aflyxe@gmail.com>
2022-12-09 09:35:38 +0000

    commit

--- script.js
+++ script.js
@@ -7,6 +7,45 @@ function myFunction() {
     text.style.display = "none";
   }
 }
+const menuBtn = document.querySelector(".menu-icon img");
+         const searchBtn = document.querySelector(".search-icon");
+         const cancelBtn = document.querySelector(".cancel-icon");
+         const items = document.querySelector(".nav-items");
+         const form = document.querySelector("form");
+         menuBtn.onclick = ()=>{
+           items.classList.add("active");
+           menuBtn.classList.add("hide");
+           searchBtn.classList.add("hide");
+           cancelBtn.classList.add("show");
+         }
+         cancelBtn.onclick = ()=>{
+           items.classList.remove("active");
+           menuBtn.classList.remove("hide");
+           searchBtn.classList.remove("hide");
+           cancelBtn.classList.remove("show");
+           form.classList.remove("active");
+           cancelBtn.style.color = "rgb(127, 126, 126)";
+         }
+         searchBtn.onclick = ()=>{
+           form.classList.add("active");
+           searchBtn.classList.add("hide");
+           cancelBtn.classList.add("show");
+         }
+
+         let subMenu = document.getElementById("subMenu");
+
+         function toggleMenu(){
+            subMenu.classList.toggle("open-menu");
+         }
+
+         var loader = document.getElementById("preloader");
+
+         window.addEventListener("load", function(){
+
+            loader.style.display = "none"
+         })
+
+         
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
