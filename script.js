bfbaa871106b497cd172e580b00275a33e7e4635
Annika Flyxe <aflyxe@gmail.com>
2022-12-09 09:20:04 +0000

    commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const menuBtn = document.querySelector(".menu-icon span");
+const menuBtn = document.querySelector(".menu-icon img");
          const searchBtn = document.querySelector(".search-icon");
          const cancelBtn = document.querySelector(".cancel-icon");
          const items = document.querySelector(".nav-items");
@@ -28,4 +28,13 @@ const menuBtn = document.querySelector(".menu-icon span");
          function toggleMenu(){
             subMenu.classList.toggle("open-menu");
          }
+
+         var loader = document.getElementById("preloader");
+
+         window.addEventListener("load", function(){
+
+            loader.style.display = "none"
+         })
+
+         
 
