c221152186325d3d4007b76d70d9a754f473d45a
Cecilie Norrbohm <ceci332s@stud.kea.dk>
2022-12-09 09:58:07 +0100

    fixes

--- subscription-page1.js
+++
@@ -1,9 +0,0 @@
-// const button = document.getElementsByClass("dd-button-2");
-
-function buttonColor() {
-  document.getElementsByClass("dd-button-2").style.backgroundColor = "#92b9ab";
-}
-
-// button.addEventListener("click", function onClick() {
-//   button.style.backgroundColor = "#92b9ab";
-// });
