bbb6751f710ab98ee1bec688f51cdfb9965e2da9
julieringaas <89411616+julieringaas@users.noreply.github.com>
2022-12-11 20:57:20 +0100

    arrow transition works

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function () {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
 
 function myFunction() {
   var checkBox = document.getElementById("myCheck");
@@ -59,16 +59,4 @@ window.addEventListener("load", function () {
   loader.style.display = "none";
 });
 
-// CHANGING COLOR form
-
-// $("input[type=text]").change(function () {
-//   var filled = true;
-//   $("input[type=text]").each(function (index) {
-//     if ($(this).val() == "") {
-//       filled = false;
-//     }
-//     if (filled === false) {
-//       //change color
-//     }
-//   });
-// });
+// rotating
